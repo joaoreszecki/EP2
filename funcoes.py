b0bb513faaac37d6c1c5a542fadedb9b17b56c64
Joao Reszecki <jreszecki@gmail.com>
2024-10-25 17:28:49 -0300

    Questão 1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,11 @@
-
+# Questão 1:
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicao=[]
+    for i in range(tamanho):
+        if orientacao == "vertical":
+            posicao.append([linha, coluna])
+            linha += 1
+        elif orientacao == "horizontal":
+            posicao.append([linha, coluna])
+            coluna += 1
+    return posicao
